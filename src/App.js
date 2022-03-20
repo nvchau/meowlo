3cbd81dda8894ffd92a366bef3f22866e33122cf
nvchau <nvchau.dev@gmail.com>
2022-03-20 16:30:24 +0700

    Clean React Architecture

--- src/App.js
+++ src/App.js
@@ -1,155 +1,17 @@
-import './App.scss';
+import React from "react"
+import './App.scss'
+
+// custom components
+import AppBar from "components/AppBar/AppBar"
+import BoardBar from "components/BoardBar/BoardBar"
+import BoardContent from "components/BoardContent/BoardContent"
 
 function App() {
   return (
     <div className="meowlo-master">
-      <nav className="navbar app">App Bar</nav>
-      <nav className="navbar board">Board Bar</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>
-              <img src="https://scontent.fdad3-6.fna.fbcdn.net/v/t1.6435-9/87877903_3147585542132267_8372581707492098048_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=e3f864&_nc_ohc=spHWolvvl1sAX_AcuLu&_nc_ht=scontent.fdad3-6.fna&oh=00_AT_5-wUztFm_G3day978tLex05OlTaFr4zZ_mRfJ5P4VoQ&oe=622983EB" alt="Chau.dev" />
-              Title: Chau.dev
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-
-        {/* https://www.youtube.com/watch?v=ZriP9yOcnvk&list=PLP6tw4Zpj-RKdGMqhYpfdl94cd4fu-RFg&index=4
-        1:16:55 */}
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
